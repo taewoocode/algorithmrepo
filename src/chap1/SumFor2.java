9784d39c357e6e68eb2fbd8955e8025d6dee7786
taewoocode <qkrxodn6035@gmail.com>
2024-04-09 21:45:49 +0900

    주석추가

--- src/chap1/SumFor2.java
+++ src/chap1/SumFor2.java
@@ -2,6 +2,10 @@ package chap1;
 
 import java.util.Scanner;
 
+/*
+입력받은 n값이 양수일 때 까지 다시 입력해야 함.
+ */
+
 public class SumFor2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner( System.in );
