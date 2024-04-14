5d042789ec87d64b14419fbe9edfd1c64ec67939
taewoocode <qkrxodn6035@gmail.com>
2024-04-14 16:35:19 +0900

    asd

--- src/chap2/PrimeNumber1.java
+++ src/chap2/PrimeNumber1.java
@@ -3,16 +3,16 @@ package chap2;
 public class PrimeNumber1 {
     public static void main(String[] args) {
         int counter = 0; // 나눗셈의 횟수
-        for (int n = 2; n <= 1000; n++) { //n이 2부터 1000까지 1씩 증가한다.
-            int i;
-            for ( i = 2; i < n; i++) { //i는 2부터 n이 증가할 때 까지 i도 1씩 증가한다.
+        int i;
+        for (int n = 2; n <= 1000; n++) { //n이 2부터 1000까지 1씩 증가한다
+            for (i = 2; i < n; i++) { //i는 2부터 n이 증가할 때 까지 i도 1씩 증가한다.
                 counter++; //나눗셈의 횟수가 증가한다.
                 if (n % i == 0) {
                     break;
                 }
-                if (n == i) {
-                    System.out.println( n );
-                }
+            }
+            if (n == i) {
+                System.out.println( n );
             }
         }
         System.out.println( "나눗셈을 수행한 횟수 :" + counter );
