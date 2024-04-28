cc05b55c1181e1a0d82b3624769ef8f14058491d
taewoocode <qkrxodn6035@gmail.com>
2024-04-28 19:21:05 +0900

    최댓값 갱신

---
+++ src/solution/Main2.java
@@ -0,0 +1,37 @@
+package solution;
+
+import java.util.Scanner;
+
+public class Main2 {
+
+    /**
+     *
+     * 인덱스 0: "it"
+     * 인덱스 1: "is"
+     * 인덱스 2: "time"
+     * 인덱스 3: "to"
+     * 인덱스 4: "study"
+     * hello, world라면 index0에는 hello가 저장이 되고, index1에는 world가 저장이 된다.
+     */
+
+    public String solution(String str) {
+        String answer = " "; //가장 긴 단어를 answer에 저장한다.
+        int max = Integer.MIN_VALUE; //가장 작은 값으로 우선 초기화를 해준다.
+        String[] s = str.split( " ");
+        for(String x : s){
+            int len = x.length();
+            if (len > max) { //기존 길이보다 더 긴 것이 발견되면
+                max = len; //길이는 더 긴 것으로 바꿔주고
+                answer = x; //단어는 교체된다.
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main2 T = new Main2();
+        Scanner scanner = new Scanner( System.in );
+        String str = scanner.nextLine();
+        System.out.println(T.solution( str ));
+    }
+}
