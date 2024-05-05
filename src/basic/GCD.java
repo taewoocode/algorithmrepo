5b398dd8867f65a96cdad54e4546f1f5e5c0ca87
taewoocode <qkrxodn6035@gmail.com>
2024-05-05 16:45:10 +0900

    최대공약수 문제

---
+++ src/basic/GCD.java
@@ -0,0 +1,34 @@
+package basic;
+
+public class GCD {
+    public static void main(String[] args) {
+
+        int num1 = 12;
+        int num2 = 18;
+
+        //두 수중에 어떤 수가 큰지 작은지 미리 판별을 해야한다 왜냐하면 num1이 18이 될 수도 있다.
+
+        int small;
+        int big;
+
+        if (num1 > num2) {
+            big = num1;
+            small = num2;
+
+        } else {
+            big = num2;
+            small = num1;
+        }
+
+        //최대공약수는 항상 1부터 시작하기 때문에 1로 초기화를 한다.
+        int gcd = 1;
+
+        //i가 1씩 증가하면서 가장 마지막에 나누어진 놈이 최대 공약수가 된다.
+        for (int i = 1; i < small; i++) {
+            if (big % i == 0 && small % i == 0) {//i로 나누었을 때 나머지가 0
+                gcd = i; // 최대공약수를 갱신 12,18일 경우에는 gcd가 6으로 갱신이 된다.
+            }
+        }
+        System.out.println( gcd );
+    }
+}
--- src/basic/ManyNumber.java
+++ src/basic/ManyNumber.java
@@ -5,33 +5,24 @@ import java.util.Scanner;
 public class ManyNumber {
     public static void main(String[] args) {
 
-/*
+
         //사용자에게 숫자를 입력받아야함
         Scanner scanner = new Scanner( System.in );
         int[] input = new int[10]; //크기가 10인 배열을 선언
         for (int i = 0; i < 10; i++) {
             input[i] = scanner.nextInt();
         }
-        //어느 숫자가 몇번 입력 받아졌는지 저장할 수 있는 자료구조가 필요하다.
 
         int[] mode = new int[10];
 
-        //ex) mode[3] == 5 -> 3이 5번 출현했다.
 
         for (int i = 0; i < 10; i++) {
             mode[input[i]]++;
         }
-*/
+    }        //ex) mode[3] == 5 -> 3이 5번 출현했다.
+    //어느 숫자가 몇번 입력 받아졌는지 저장할 수 있는 자료구조가 필요하다.
+    //만약 input[1] = 7이라면, mode 배열에서 인덱스 7에 해당하는 값을 1 증가시킬 것입니다. 즉, 숫자 7이 입력된 횟수를 기록하는 것이죠.
 
-        char v1 = 'A';
 
-        if(v1 == 'A'){
-           int v2 = 100;
-           double v3 = 3.14;
 
-        }
-
-        boolean v4 = true;
-
-    }
 }
