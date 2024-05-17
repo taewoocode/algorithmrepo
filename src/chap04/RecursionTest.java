dfbeccf6ec8451a6d48a3580014abc9bc7367ab8
taewoocode <qkrxodn6035@gmail.com>
2024-05-17 16:57:59 +0900

    asd

--- src/chap04/RecursionTest.java
+++ src/chap04/RecursionTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class RecursionTest {
 
     @Test
-    @DisplayName( "문자열테스트" )
+    @DisplayName("문자열테스트")
     public void test() {
         String str = "Hello world"; //문자열을 str에 담기
         char[] chars = str.toCharArray(); //한글자식 분리해서 chars에 담기
@@ -20,7 +20,7 @@ class RecursionTest {
     }
 
     @Test
-    @DisplayName( "StringTest" )
+    @DisplayName("StringTest")
     public void stringTest() {
         String str = "Hello world";
         List<String> list = new ArrayList<>();
@@ -30,4 +30,31 @@ class RecursionTest {
         System.out.println( list );
     }
 
+    @Test
+    @DisplayName("문자열 테스트 하기")
+    public void StringTest2() {
+        String str = "HEllO BOY";
+        List<String> list = new ArrayList<>();
+        for (String s : str.split( "" )) {
+            list.add( s );
+        }
+        System.out.println( list );
+    }
+
+    @Test
+    @DisplayName("문자열 뒤집기")
+    public void testMain() {
+        String str = "SpdECar";
+        char[] chars = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : chars) {
+            if (Character.isLowerCase( c )) { //소문자면
+                sb.append( Character.toUpperCase( c ) ); //대문자로 전환
+            }else {
+                sb.append( Character.toLowerCase( c ) ); //대문자면 소문자로 전환
+            }
+        }
+        System.out.println( sb.toString() ); //객체가 가지고 있는 정보를 문자열로 만들어서 반환
+    }
 }
