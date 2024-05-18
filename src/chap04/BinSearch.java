06c0e6b5bd21acc2e5eef640ac7ea7fa46e0df8f
taewoocode <qkrxodn6035@gmail.com>
2024-05-19 02:08:54 +0900

    seqSearch

--- src/chap04/BinSearch.java
+++ src/chap04/BinSearch.java
@@ -41,4 +41,16 @@ public class BinSearch {
         }
 
     }
+
+    static int seqSearch(int[] a, int n, int key) {
+        int i = 0;
+
+        while (i < n) {
+            if (a[i] == key) {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
 }
